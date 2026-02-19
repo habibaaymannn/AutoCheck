8a78fcfb279b2bfc79da269edab55c83eaaf7ccd
Maria Alfons <mariaalfonse206@gmail.com>
2026-02-19 14:20:32 +0200

    config

---
+++ config/ML.py
@@ -0,0 +1,8 @@
+from YamlObj import YamlObj
+from dataclasses import dataclass, field
+
+@dataclass
+class ML(YamlObj):
+    def __init__(self):
+        pass
+
---
+++ config/YamlObj.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class YamlObj(ABC):
+    @abstractmethod
+    def validate(self) -> bool:
+        pass
