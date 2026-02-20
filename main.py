d0c9883312d2899adbc4f189b6fc9d2a03378a7a
Toqa781 <abdallatoqa56@gmail.com>
2026-02-20 22:26:12 +0200

    good looking error message

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from config.ConfigManager import ConfigManager
+from config.ConfigManager import ConfigManager, ConfigParseError, ConfigValidationError
+import sys
 
-cm = ConfigManager()
-cm.parse("config.yaml")
-cm.validate()
-
-print("Config loaded successfully!")
-print("Mode:", cm.mode)
-print("Configs:", cm.configs)
+if __name__ == "__main__":
+    cm = ConfigManager()
+    try:
+        cm.parse("config.yaml")
+        cm.validate()
+        print("Config loaded successfully!")
+    except (ConfigParseError, ConfigValidationError) as e:
+        msg = str(e).replace("\n", " | ")
+        print(f"❌[CONFIG ERROR] Unexpected configuration failure.\n - {msg}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"❌[CONFIG ERROR] Unexpected configuration failure.\n - {e}")
+        sys.exit(1)
