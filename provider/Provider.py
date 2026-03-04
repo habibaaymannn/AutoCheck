1e4c699856ba5762960fb51bbc1ddcfd604625b5
Maria Alfons <mariaalfonse206@gmail.com>
2026-03-04 16:34:33 +0200

    Modified loggers' calls

--- provider/Provider.py
+++ provider/Provider.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 class Provider():
     def __init__(self):
         pass
-    def fetch_ckpnt(self) -> Dict[str, Any]:
+    def fetch_ckpt(self) -> Dict[str, Any]:
         pass
     def fetch_all(self):
         pass
